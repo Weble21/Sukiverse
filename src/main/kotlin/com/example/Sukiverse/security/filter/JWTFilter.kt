f1e70f64bf7db612d1bcc28c7e06e591b01301a4
Weble21 <89642807+Weble21@users.noreply.github.com>
2026-05-30 17:42:29 +0900

    fix JWT verification

--- src/main/kotlin/com/example/Sukiverse/security/filter/JWTFilter.kt
+++ src/main/kotlin/com/example/Sukiverse/security/filter/JWTFilter.kt
@@ -8,6 +8,9 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import org.springframework.util.AntPathMatcher
 import org.springframework.web.filter.OncePerRequestFilter
@@ -44,6 +47,10 @@ class JWTFilter(
 
         try {
             jwtProvider.verifyToken(authHeader.substring(7))
+            val auth = UsernamePasswordAuthenticationToken(
+                authHeader.substring(7), null, listOf(SimpleGrantedAuthority("ROLE_USER"))
+            )
+            SecurityContextHolder.getContext().authentication = auth
         } catch (e: CustomException) {
             writeError(response, e.getHttpStatus(), e.getErrorCode(), e.getCodeInterface().message)
             return
